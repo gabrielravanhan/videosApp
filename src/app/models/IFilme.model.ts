35d64b8db612e65bcb1f55d9e36d1a1503fffb24
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-05-31 22:24:15 -0300

    Adicionei o array de filmes e a interface de filme

---
+++ src/app/models/IFilme.model.ts
@@ -0,0 +1,8 @@
+export interface IFilme {
+  nome: string;
+  lancamento: string;
+  duracao: string;
+  classificacao: number;
+  cartaz: string;
+  generos: string[];
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,37 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'O Exterminador do Futuro (1984)',
+      lancamento: '25/03/1985',
+      duracao: '1h 47m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fo4SSoNT1ASI4JP7dOLrosWNWKa.jpg',
+      generos: ['Ação', 'Thriller', 'Ficção científica']
+    },
+
+    {
+      nome: 'Rambo: Programado Para Matar (1982)',
+      lancamento: '06/11/1982',
+      duracao: '1h 34m',
+      classificacao: 74,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/6ocBqNSqG1hWbzjv2sZxLc07nLs.jpg',
+      generos: ['Ação', 'Aventura', 'Thriller', 'Guerra']
+    },
+
+    {
+      nome: 'Kickboxer: O Desafio do Dragão (1989)',
+      lancamento: '09/09/1989',
+      duracao: '1h 37m',
+      classificacao: 68,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/e50XYhyoaJpJUNn18Fs4FsZgtfx.jpg',
+      generos: ['Ação', 'Thriller']
+    },
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) { }
 
