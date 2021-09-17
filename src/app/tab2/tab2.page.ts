3ba64ae3467c83a2ed6c060980629685782d8234
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-09-17 15:27:45 -0300

    Componentes programáveis no tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) { }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'OLÁ, NENÉNS!!!',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'OLÁ, NENÉNS!!!',
+      duration: 2000,
+      color: 'azul-escuro'
+    });
+    toast.present();
+  }
 
 }
